c96fc70bf9db95254ba7b0da007d0c061cd8dc05
AntonioVillegas13 <antonio.villegas@epn.edu.ec>
2022-06-08 18:10:26 -0500

    ola

---
+++ laraweb_Dropbox-master/app/Providers/DropboxServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Providers;
+
+
+use Illuminate\Filesystem\FilesystemAdapter;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\ServiceProvider;
+use League\Flysystem\Filesystem;
+use Spatie\Dropbox\Client as DropboxClient;
+use Spatie\FlysystemDropbox\DropboxAdapter;
+
+class DropboxServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+
+    public function boot()
+    {
+        
+    }
+}
